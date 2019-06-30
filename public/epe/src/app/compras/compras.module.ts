6bc5044904d729f0fec6a5d47c4f661e87d67edb
cachilupi <carlosmoralesduarte97@gmail.com>
2019-06-30 19:50:16 -0400

    menu side en todas

--- public/epe/src/app/compras/compras.module.ts
+++ public/epe/src/app/compras/compras.module.ts
@@ -1,25 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
 import { ComprasPage } from './compras.page';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: ComprasPage
-  }
-];
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild([
+      {
+        path: '',
+        component: ComprasPage
+      }
+    ])
   ],
   declarations: [ComprasPage]
 })
